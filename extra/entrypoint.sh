8e9b1460f61fb2ea34c2292983e10026938a76e8
Jacson Santos <jacson.santos@phonetrack.com.br>
2024-09-20 16:04:15 -0300

    alterar arquivo env

--- extra/entrypoint.sh
+++ extra/entrypoint.sh
@@ -1,6 +1,20 @@
 #!/usr/bin/env bash
 set -x
 
+if [ ! -f .env ]; then
+    cp .env.example .env
+    php artisan key:generate
+
+    # Caminho para o arquivo .env
+    ENV_FILE=".env"
+
+    # Substituições
+    sed -i 's/APP_NAME=Laravel/APP_NAME=Exemplo/' $ENV_FILE
+    sed -i 's/APP_ENV=local/APP_ENV=production/' $ENV_FILE
+    sed -i 's/APP_DEBUG=true/APP_DEBUG=false/' $ENV_FILE
+    sed -i 's|APP_URL=http://localhost|APP_URL=http://contatopesqquisa.online|' $ENV_FILE
+fi
+
 php artisan migrate
 
 apachectl -D FOREGROUND
